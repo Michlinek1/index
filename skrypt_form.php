2ed3e506e6791ea6959ebe934581e7f93d069c12
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-04-10 16:32:36 +0200

    Update skrypt_form.php

--- skrypt_form.php
+++ skrypt_form.php
@@ -11,23 +11,15 @@ if (mysqli_connect_error()) {
   die("Nie Połączono" . mysqli_connect_error())."<br>";
 }
 echo "Połączono! <br>";
-$tabelka =  mysqli_query($pol, "SELECT 1 FROM dane LIMIT 1" );
-if($tabelka)
-{
-   echo "Tabelka już istnieje";
-}else{
-  echo "Tabelka nie istnieje";
-  echo $pol -> error;
-  $sql -> query("create TABLE dane (
-    imie VARCHAR(20),
-    nazwisko VARCHAR(20),
-    zawod VARCHAR(20),
-    mail VARCHAR(20),
-    wyksztalcenie VARCHAR(20)
-  )");
+//$tabelka =  mysqli_query($pol, "SELECT 1 FROM dane LIMIT 1" );
+$sql  = mysqli_query($pol, "create table if not exists dane (
+  imie VARCHAR(20),
+  nazwisko VARCHAR(20),
+  zawod VARCHAR(20),
+  mail VARCHAR(20),
+  wyksztalcenie VARCHAR(20)
+)");
 
-}
- 
 
 
 
