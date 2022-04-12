9b4245c233e6324262644700bd6eaa124bfc6921
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-04-12 17:12:05 +0200

    Update usuwanie.php

--- usuwanie.php
+++ usuwanie.php
@@ -5,44 +5,46 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Usuwanie</title>
+<body style="text-align: center;">
+  Co chcesz usunac? Wpisz ID <br>
+  <form action = "" method = "post">
+  <input type="text" name = "nazw" value=" "size="30"><br>
+  <input type="submit" value = "Usuwanie" name = "usun" >
+  </form>
+  <body>
+</html>
 <?php
+$textbox1 = $_POST['nazw'];	
+$przycisk = $_POST['usun'];
 error_reporting(1);
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$nazw = $_POST['nazw'];
-$im= $_POST['im'];
-$zaw= $_POST['zaw'];
-$adr= $_POST['adr'];
-$checkbox = $_POST['opcje'];
 $pol = new mysqli("localhost", "root", "", "baza");
 if (mysqli_connect_error()) {
   die("Nie Połączono" . mysqli_connect_error())."<br>";
 }
-echo "Połączono! <br>";
-$sql  = mysqli_query($pol, "create table if not exists dane (
-  ID int NOT NULL  AUTO_INCREMENT,
-  imie VARCHAR(20)  NOT NULL,
-  nazwisko VARCHAR(20)  NOT NULL,
-  zawod VARCHAR(20)  NOT NULL,
-  mail VARCHAR(40)  NOT NULL,
-  wyksztalcenie VARCHAR(20)  NOT NULL,
-  PRIMARY KEY (ID)
-)");
+
+$sql = "SELECT ID,imie, nazwisko, zawod, mail, wyksztalcenie FROM dane";
+$result = $pol->query($sql);
 
 
-if($checkbox == "on"){
-  mysqli_query($pol, "INSERT INTO dane(imie, nazwisko, zawod, mail, wyksztalcenie)
-  Values ('$im', '$nazw','$zaw','$adr', 'tak')");
-  echo "Zapisano!";
-  
-}else{
-  $sqldodawanie = mysqli_query($pol, "INSERT INTO dane(imie, nazwisko, zawod, mail, wyksztalcenie)
-Values ('$im', '$nazw','$zaw','$adr', 'nie')");
- echo "Zapisano!";
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      echo "ID" . $row["ID"]." "."Imie: " . $row["imie"]. " ".  "Nazwisko: " . $row["nazwisko"]. " " . "Zawód:". $row["zawod"]." ". "Mail:".$row["mail"]. " ". "Wykształcenie"." ".$row["wyksztalcenie"]. "<br>";
+    }
+ } else {
+header("Location: index.php");
+ }
+ echo $result->num_rows."<br>";
+
+if(isset($przycisk)){
+  mysqli_query($pol, "DELETE FROM dane where id = $textbox1");
+
+}elseif($result->num_rows  < $textbox1){
+  echo "Taki record nie istnieje!";
 }
 
 
 
-$pol->close()
 ?>
-<form action = "index.php" method = "post">
-<input type = "submit" value = "wyjdz" name = "wyjdz">
+
+
